93719534dd03a8f1a8aa19b3ee74f6ab788871b9
Kamilkhan421 <aa1343@srmist.edu.in>
2025-07-26 10:54:55 +0530

    Initial commit

--- task-scheduler-api/task-scheduler-api/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE task (
-  id SERIAL PRIMARY KEY,
-  task_str_id VARCHAR(255) UNIQUE NOT NULL,
-  description TEXT NOT NULL,
-  estimated_time_minutes INTEGER NOT NULL CHECK (estimated_time_minutes > 0),
-  status VARCHAR(20) DEFAULT 'pending' CHECK (status IN ('pending', 'processing', 'completed')),
-  submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
